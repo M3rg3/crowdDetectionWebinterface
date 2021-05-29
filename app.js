feba2274495ad31bac4590107ee5835bc91c6a01
Jordan <Jordangurren@hotmail.co.uk>
2021-05-29 18:06:33 +0100

    make it better

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const uri = "mongodb+srv://Read-only:Password123456789@cluster0.kxfj4.mongodb.ne
 
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname, 'build', "/index.html"));
     console.log("Host has connected to application!");
 })
 
